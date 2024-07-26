d8f959fbe8a167d2dadb991c26bbda690c748911
DEEPAK-SINGH-GOOR <149694925+DEEPAK-SINGH-GOOR@users.noreply.github.com>
2024-07-26 10:15:45 +0530

    JavaScript-Final-Project

--- JBL-Webiste/navigation.js
+++ JBL-Webiste/navigation.js
@@ -1,3 +1,5 @@
+
+
 // Wait for the DOM content to load
 document.addEventListener('DOMContentLoaded', function () {
     const signInForm = document.getElementById('signInForm');
@@ -13,4 +15,29 @@ document.addEventListener('DOMContentLoaded', function () {
         window.location.href = 'index.html';
         alert('Sign In Successful !!');
     });
+    document.getElementById('signInForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const username = document.getElementById('username').value;
+    const emailOrPhone = document.getElementById('emailOrPhone').value;
+    const password = document.getElementById('password').value;
+
+    if (username && emailOrPhone && password) {
+       
+        localStorage.setItem('isUserSignedUp', 'true');
+        window.location.href = 'product.html';
+    } else {
+        alert('Please fill in all fields.');
+    }
 });
+
+function checkSignInStatus() {
+    if (localStorage.getItem('isUserSignedUp') !== 'true') {
+        window.location.href = 'sign.html';
+    }
+}
+
+document.addEventListener('DOMContentLoaded', checkSignInStatus);
+
+});
+
