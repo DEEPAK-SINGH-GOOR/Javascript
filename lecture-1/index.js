1c3ad6fd715bcf33062dc4576ff9e570cc0299b8
DEEPAK-SINGH <149694925+DEEPAK-SINGH-GOOR@users.noreply.github.com>
2025-07-21 09:11:10 +0530

    Update index.js

--- lecture-1/index.js
+++ lecture-1/index.js
@@ -1,2 +1,27 @@
 console.log("deepak hello javascript");
 
+let text = "w57 t2v 5v e59";
+let arr = text.split(" ");
+let final = [];
+
+for (let i = 0; i < arr.length; i++) {
+  let str = arr[i];
+  let letter = "";
+  let num = "";
+
+  for (let j = 0; j < str.length; j++) {
+    if (isNaN(str[j])) {
+      letter += str[j];
+    } else {
+      num += str[j];
+    }
+  }
+
+  final.push([letter, Number(num)]);
+}
+
+final.sort(); // sort by letters
+
+let onlyNumber = final.map(x => x[1]);
+
+console.log("Only Numbers:", onlyNumber); // [59, 5, 2, 57]
