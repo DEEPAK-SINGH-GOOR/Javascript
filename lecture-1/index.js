dbba7cc234fa642f7d6815f8ddb7d13e6363019e
DEEPAK-SINGH <149694925+DEEPAK-SINGH-GOOR@users.noreply.github.com>
2025-07-22 15:26:10 +0530

    Update index.js

--- lecture-1/index.js
+++ lecture-1/index.js
@@ -1,28 +1,37 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Dropdown Log Example</title>
+  <title>Head Tail Task</title>
 </head>
 <body>
 
-  <label for="mySelect">Choose one:</label>
-  <select id="mySelect">
-    <option value="">-- Select Option --</option>
-    <option value="A">A</option>
-    <option value="B">B</option>
-    <option value="C">C</option>
-    <option value="D">D</option>
-  </select>
+  <button onclick="addValue('H')">H</button>
+  <button onclick="addValue('T')">T</button>
+
+  <div id="container"></div>
 
   <script>
-    const dropdown = document.getElementById("mySelect");
+    let last = "";
+    let currentRow = null;
 
-    dropdown.addEventListener("change", function () {
-      const selectedValue = dropdown.value;
-      if (selectedValue) {
-        console.log("You selected:", selectedValue);
+    function addValue(val) {
+      if (val !== last) {
+        // Create new row if different
+        currentRow = document.createElement("div");
+        currentRow.style.display = "flex";
+        currentRow.style.margin = "5px";
+        document.getElementById("container").appendChild(currentRow);
       }
-    });
+
+      let box = document.createElement("div");
+      box.innerText = val;
+      box.style.border = "1px solid black";
+      box.style.padding = "10px";
+      box.style.marginRight = "5px";
+      currentRow.appendChild(box);
+
+      last = val;
+    } 
   </script>
 
 </body>
