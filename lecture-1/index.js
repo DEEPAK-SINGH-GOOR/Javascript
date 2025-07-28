851b772dd6926474e8f21a43b20340f3973c6b88
DEEPAK-SINGH <149694925+DEEPAK-SINGH-GOOR@users.noreply.github.com>
2025-07-28 15:39:19 +0530

    Update index.js

--- lecture-1/index.js
+++ lecture-1/index.js
@@ -1,74 +1,16 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Simple </title>
-</head>
-<body>
+let string = "i am farming";
+let cleanStr = string.replace(/\s/g, '');
+let chars = cleanStr.split('');
 
-  <select id="country">
-    <option>Select Country</option>
-  </select>
+let count = {};
 
-  <select id="state">
-    <option>Select State</option>
-  </select>
+chars.forEach(char => {
+  count[char] = (count[char] || 0) + 1;
+});
 
-  <select id="city">
-    <option>Select City</option>
-  </select>
+let result = Object.keys(count).map(char => ({
+  name: char,
+  occurrence: count[char]
+}));
 
-  <script>
-    async function setupDropdowns() {
-      let res = await fetch("https://countriesnow.space/api/v0.1/countries/states");
-      let json = await res.json();
-      let data = json.data;
-
-      let countrySelect = document.getElementById("country");
-      let stateSelect = document.getElementById("state");
-      let citySelect = document.getElementById("city");
-
-      data.forEach(country => {
-        let opt = document.createElement("option");
-        opt.textContent = country.name;
-        countrySelect.append(opt);
-      });
-
-      countrySelect.addEventListener("change", function () {
-        stateSelect.innerHTML = "<option>Select State</option>";
-        citySelect.innerHTML = "<option>Select City</option>";
-        let selected = this.value;
-        let found = data.find(c => c.name === selected);
-        if (found) {
-          found.states.forEach(state => {
-            let opt = document.createElement("option");
-            opt.textContent = state.name;
-            stateSelect.append(opt);
-          });
-        }
-      });
-
-      stateSelect.addEventListener("change", async function () {
-        citySelect.innerHTML = "<option>Select City</option>";
-        let country = countrySelect.value;
-        let state = stateSelect.value;
-
-        let cityRes = await fetch("https://countriesnow.space/api/v0.1/countries/state/cities", {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({ country, state })
-        });
-
-        let cityJson = await cityRes.json();
-        cityJson.data.forEach(city => {
-          let opt = document.createElement("option");
-          opt.textContent = city;
-          citySelect.append(opt);
-        });
-      });
-    }
-
-    setupDropdowns();
-  </script>
-
-</body>
-</html>
+console.log(result);
