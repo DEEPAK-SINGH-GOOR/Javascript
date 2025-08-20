8193eb122f97859a3de4d0f5b2ea1c8391122e61
DEEPAK-SINGH <149694925+DEEPAK-SINGH-GOOR@users.noreply.github.com>
2025-08-20 07:40:25 +0530

    Update index.js

--- lecture-1/index.js
+++ lecture-1/index.js
@@ -1,10 +1,9 @@
-import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
 import { useDispatch } from "react-redux";
-import { login } from "../features/userSlice"; // using login reducer to store user
-
-const Signup = () => {
-  const [user, setUser] = useState({
+import { logout } from "../features/userSlice";
+const Logout = () => {
+  const dispatch = useDispatch();
+  let User = {
     firstName: "",
     lastName: "",
     email: "",
@@ -12,253 +11,315 @@ const Signup = () => {
     country: "",
     gender: "",
     language: [],
-  });
+  };
+  const [user, setUser] = useState(User);
 
   const [error, setError] = useState({});
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
 
-  //  handle input
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setUser({ ...user, [name]: value });
-  };
-
-  // handle checkbox (language)
-  const handleLanguage = (e) => {
-    const { value, checked } = e.target;
-    let updated = [...user.language];
-    if (checked) {
-      updated.push(value);
-    } else {
-      updated = updated.filter((lang) => lang !== value);
-    }
-    setUser({ ...user, language: updated });
-  };
-
-  //  validation
   const validate = () => {
     let newError = {};
-    if (!user.firstName) newError.firstName = "First name required";
-    if (!user.lastName) newError.lastName = "Last name required";
-    if (!user.email) newError.email = "Email required";
-    else if (!/\S+@\S+\.\S+/.test(user.email))
-      newError.email = "Invalid email";
-    if (!user.password) newError.password = "Password required";
-    if (!user.country) newError.country = "Country required";
-    if (!user.gender) newError.gender = "Gender required";
-    if (user.language.length === 0)
-      newError.language = "At least one language required";
 
+    if (!user.firstName) {
+      newError.firstName = "First name required";
+    } else if (!/^[A-Za-z]+$/.test(user.firstName)) {
+      newError.firstName = "Only alphabets allowed";
+    }
+
+    if (!user.lastName) {
+      newError.lastName = "Last name required";
+    } else if (!/^[A-Za-z]+$/.test(user.lastName)) {
+      newError.lastName = "Only alphabets allowed";
+    }
+
+    if (!user.email) {
+      newError.email = "Email is required";
+    } else if (!/^\S+@\S+\.\S+$/.test(user.email)) {
+      newError.email = "Invalid email format";
+    }
+
+    if (!user.password) {
+      newError.password = "Password is required";
+    } else if (user.password.length < 6) {
+      newError.password = "Password must be at least 6 characters";
+    }
+
+    if (!user.dob) {
+      newError.dob = "Date of birth required";
+    }
+
+    if (!user.contact) {
+      newError.contact = "Contact number required";
+    } else if (!/^[0-9]{10}$/.test(user.contact)) {
+      newError.contact = "Contact must be 10 digits";
+    }
+
+    if (!user.country) {
+      newError.country = "Country is required";
+    }
+
+    if (!user.gender) {
+      newError.gender = "Gender is required";
+    }
+
+    if (user.language.length === 0) {
+      newError.language = "At least one language required";
+    }
+
+    console.log(Object.keys(newError).length, "length");
     setError(newError);
     return Object.keys(newError).length === 0;
   };
 
-  //  submit
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    console.log(name, "NAME");
+    console.log(value, "value");
+
+    setUser({ ...user, [name]: value });
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
     if (!validate()) return;
-
-    // store user in redux (same as login)
-    dispatch(login(user));
-
-    // redirect to Home
-    navigate("/home");
+    dispatch(signup(user));
+    console.log("working");
   };
 
+  const handleLogout = (e) => {
+    e.preventDefault();
+    dispatch(logout());
+  };
   return (
     <div>
-      <div className="flex justify-center pt-25 ">
-        <div className="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow-sm sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
-          <form className="space-y-6" onSubmit={handleSubmit}>
-            <h4 className="text-3xl font-medium text-gray-900 dark:text-white">
-              Signup
-            </h4>
-            <hr className="bg-white" />
+      {/* <button
+        onClick={(e) => handleLogout(e)}
+        className="px-5 py-3 text-base font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      >
+        Logout
+      </button> */}
+ <nav className="relative bg-gray-800 after:pointer-events-none after:absolute after:inset-x-0 after:bottom-0 after:h-px after:bg-white/10 ">
+  <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+    <div className=" flex h-16 items-center justify-end">
+      <button
+        onClick={(e) => handleLogout(e)}
+        className="px-5 py-3 text-base font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      >
+        Logout
+      </button>
+    </div>
+  </div>
+</nav> 
 
-            {/* First / Last Name */}
-            <div className="flex justify-center gap-4">
-              <div>
-                <label className="block mb-2 text-sm">Enter First Name</label>
-                <input
-                  type="text"
-                  name="firstName"
-                  value={user.firstName}
-                  onChange={handleChange}
-                  placeholder="First Name"
-                  className="bg-gray-50 border rounded-lg p-2.5 w-full"
-                />
-                {error.firstName && (
-                  <p className="text-red-500 text-sm">{error.firstName}</p>
-                )}
-              </div>
-              <div>
-                <label className="block mb-2 text-sm">Enter Last Name</label>
-                <input
-                  type="text"
-                  name="lastName"
-                  value={user.lastName}
-                  onChange={handleChange}
-                  placeholder="Last Name"
-                  className="bg-gray-50 border rounded-lg p-2.5 w-full"
-                />
-                {error.lastName && (
-                  <p className="text-red-500 text-sm">{error.lastName}</p>
-                )}
-              </div>
-            </div>
+      <div className="flex flex-col justify-center sm:h-screen p-4 ">
+        <div className="max-w-md w-full mx-auto border border-gray-300 rounded-2xl p-8">
+          <div className="text-center mb-12">
+            <h1 className="text-3xl font-bold">Registration Form</h1>
+          </div>
+          <form onSubmit={handleSubmit}>
 
-            {/* Email */}
-            <div>
-              <label className="block mb-2 text-sm">Enter Email</label>
+            <div className="space-y-6">
+              <label className="text-slate-900 text-sm font-medium mb-2 block">
+                First Name
+              </label>
               <input
+                name="firstName"
                 type="text"
+                value={user.firstName}
+                onChange={handleChange}
+                className="text-slate-900 bg-white border border-gray-300 w-full text-sm px-4 py-3 rounded-md outline-blue-500"
+                placeholder="Enter First Name"
+              />
+              <p className="mt-2 text-sm text-red-600">{error.firstName}</p>
+            </div>
+            <br />
+
+            <div className="space-y-6">
+              <label className="text-slate-900 text-sm font-medium mb-2 block">
+                Last Name
+              </label>
+              <input
+                name="lastName"
+                type="text"
+                value={user.lastName}
+                onChange={handleChange}
+                className="text-slate-900 bg-white border border-gray-300 w-full text-sm px-4 py-3 rounded-md outline-blue-500"
+                placeholder="Enter Last Name"
+              />
+              <p className="mt-2 text-sm text-red-600">{error.lastName}</p>
+            </div>
+            <br />
+
+            <div className="space-y-6">
+              <label className="text-slate-900 text-sm font-medium mb-2 block">
+                Email
+              </label>
+              <input
                 name="email"
+                type="email"
                 value={user.email}
                 onChange={handleChange}
-                placeholder="Enter email..."
-                className="bg-gray-50 border rounded-lg p-2.5 w-full"
+                className="text-slate-900 bg-white border border-gray-300 w-full text-sm px-4 py-3 rounded-md outline-blue-500"
+                placeholder="Enter Email"
               />
-              {error.email && (
-                <p className="text-red-500 text-sm">{error.email}</p>
-              )}
+              <p className="mt-2 text-sm text-red-600">{error.email}</p>
             </div>
+            <br />
 
-            {/* Password */}
-            <div>
-              <label className="block mb-2 text-sm">Enter Password</label>
-              <input
-                type="password"
-                name="password"
-                value={user.password}
-                onChange={handleChange}
-                placeholder="Enter password"
-                className="bg-gray-50 border rounded-lg p-2.5 w-full"
-              />
-              {error.password && (
-                <p className="text-red-500 text-sm">{error.password}</p>
-              )}
+            <div className="sm:flex gap-5">
+              <div className="w-[180px]">
+                <label className="text-slate-900 text-sm font-medium mb-2 block">
+                  Date Of Birth
+                </label>
+                <input
+                  name="dob"
+                  type="date"
+                  value={user.dob}
+                  onChange={handleChange}
+                  className="text-slate-900 bg-white border border-gray-300 w-full text-sm px-4 py-3 rounded-md outline-blue-500"
+                />
+                <p className="mt-2 text-sm text-red-600">{error.dob}</p>
+              </div>
+
+              <div className="w-[180px]">
+                <label className="text-slate-900 text-sm font-medium mb-2 block">
+                  Contact No.
+                </label>
+                <input
+                  name="contact"
+                  type="text"
+                  value={user.contact}
+                  onChange={handleChange}
+                  placeholder="Enter Contact No."
+                  className="text-slate-900 bg-white border border-gray-300 w-full text-sm px-4 py-3 rounded-md outline-blue-500"
+                />
+                <p className="mt-2 text-sm text-red-600">{error.contact}</p>
+              </div>
             </div>
+            <br />
 
-            {/* Country */}
-            <div>
-              <label className="block mb-2 text-sm">Select Country</label>
-              <select
-                name="country"
-                value={user.country}
-                onChange={handleChange}
-                className="bg-gray-50 border rounded-lg p-2.5 w-full"
-              >
-                <option value="" disabled>
-                  Choose a country
-                </option>
-                <option value="US">United States</option>
-                <option value="CA">Canada</option>
-                <option value="FR">France</option>
-                <option value="DE">Germany</option>
-              </select>
-              {error.country && (
-                <p className="text-red-500 text-sm">{error.country}</p>
-              )}
-            </div>
+            <div className="flex justify-center gap-6">
+              <div className="w-[180px]">
+                <label className="block mb-2 text-sm font-medium text-gray-900">
+                  Select Country
+                </label>
+                <select
+                  name="country"
+                  value={user.country}
+                  onChange={handleChange}
+                  className="text-slate-900 bg-white border border-gray-300 w-full text-sm px-4 py-3 rounded-md outline-blue-500"
+                >
+                  <option value="">Choose a country</option>
+                  <option value="usa">USA</option>
+                  <option value="india">India</option>
+                  <option value="china">China</option>
+                </select>
+                <p className="mt-2 text-sm text-red-600">{error.country}</p>
+              </div>
 
-            {/* Gender */}
-            <div>
-              <label className="block mb-2 text-sm">Select Gender</label>
-              <select
-                name="gender"
-                value={user.gender}
-                onChange={handleChange}
-                className="bg-gray-50 border rounded-lg p-2.5 w-full"
-              >
-                <option value="" disabled>
+              <div className="w-[180px]">
+                <label className="block mb-2 text-sm font-medium text-gray-900">
                   Select Gender
-                </option>
-                <option value="Male">Male</option>
-                <option value="Female">Female</option>
-              </select>
-              {error.gender && (
-                <p className="text-red-500 text-sm">{error.gender}</p>
-              )}
+                </label>
+                <select
+                  name="gender"
+                  value={user.gender}
+                  onChange={handleChange}
+                  className="text-slate-900 bg-white border border-gray-300 w-full text-sm px-4 py-3 rounded-md outline-blue-500"
+                >
+                  <option value="">Select Gender</option>
+                  <option value="male">Male</option>
+                  <option value="female">Female</option>
+                </select>
+                <p className="mt-2 text-sm text-red-600">{error.gender}</p>
+              </div>
             </div>
+            <br />
 
-            {/* Languages */}
-            <div className="flex gap-2">
-              <label className="text-sm">Select Language:</label>
+            <div className="flex items-center gap-3">
+              <label className="text-slate-900 text-sm font-medium block w-full">
+                Select Language:
+              </label>
+
               <input
                 type="checkbox"
+                name="language"
                 value="English"
                 checked={user.language.includes("English")}
-                onChange={handleLanguage}
+                onChange={handleChange}
               />
               <span>English</span>
+
               <input
                 type="checkbox"
+                name="language"
                 value="Hindi"
                 checked={user.language.includes("Hindi")}
-                onChange={handleLanguage}
+                onChange={handleChange}
               />
               <span>Hindi</span>
+
               <input
                 type="checkbox"
+                name="language"
                 value="Gujarati"
                 checked={user.language.includes("Gujarati")}
-                onChange={handleLanguage}
+                onChange={handleChange}
               />
               <span>Gujarati</span>
             </div>
-            {error.language && (
-              <p className="text-red-500 text-sm">{error.language}</p>
-            )}
+            <p className="mt-2 text-sm text-red-600">{error.language}</p>
 
-            {/* Button */}
-            <button
-              type="submit"
-              className="w-full text-white bg-blue-700 hover:bg-blue-800 rounded-lg px-5 py-2.5"
-            >
-              Signup
-            </button>
-
-            <div className="text-sm text-gray-500">
-              Already have account?{" "}
-              <Link to="/login" className="text-blue-600 hover:underline">
-                Login
-              </Link>
+            <div className="mt-12">
+              <button
+                type="submit"
+                className="w-full py-3 px-4 text-sm tracking-wider font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none cursor-pointer"
+              >
+                Submit
+              </button>
             </div>
           </form>
+
         </div>
+      </div>
+      <br />
+      <div className="relative overflow-x-auto shadow-md sm:rounded-lg   ">
+        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                First name
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Last Name
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Email
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Date Of Birth
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Contact
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Country
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Gender
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Language
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Action
+              </th>
+            </tr>
+          </thead>
+          <tbody></tbody>
+        </table>
       </div>
     </div>
   );
 };
 
-export default Signup;
-//import { createSlice } from "@reduxjs/toolkit";
-
-export const userSlice = createSlice({
-  name: "user",
-  initialState: {
-    user: null,
-  },
-  reducers: {
-    signup: (state, action) => {
-      state.user = action.payload;
-      console.log("Signup Success:", state.user);
-    },
-    login: (state, action) => {
-      state.user = action.payload;
-      console.log("Login Success:", state.user);
-    },
-    logout: (state) => {
-      state.user = null;
-      console.log("User Logged Out");
-    },
-  },
-});
-
-export const { signup, login, logout } = userSlice.actions;
-
-// selector
-export const selectUser = (state) => state.user.user;
-
-export default userSlice.reducer;
+export default Logout;
